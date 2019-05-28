4fc5d64a2e5ed937ae2bc89c36a69531a16280b8
Ananya Singh <as2863@njit.edu>
2019-05-28 15:33:38 -0400

    still updating

--- js/init.js
+++ js/init.js
@@ -538,7 +538,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "UI/UX Designer", "Web Developer"],
+			strings: ["Software Engineer", "Back-End Engineer", "Web Developer", "Data Scientist"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
